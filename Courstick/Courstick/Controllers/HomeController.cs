c17b96740a28bb6663d94cde69c1dcc4305e2e1a
robert <78978033+robertGGA@users.noreply.github.com>
2022-03-24 17:28:26 +0300

    added reg page

--- Courstick/Courstick/Controllers/HomeController.cs
+++ Courstick/Courstick/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Courstick.Controllers
             return View();
         }
 
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

14e3c48181b5019cfd1f893ab607b89497fc4d9e
robert <78978033+robertGGA@users.noreply.github.com>
2022-03-26 12:24:59 +0300

    added course page

--- Courstick/Courstick/Controllers/HomeController.cs
+++ Courstick/Courstick/Controllers/HomeController.cs
@@ -38,7 +38,27 @@ namespace Courstick.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Course()
+        {
+            return View();
+        }
+
+        public IActionResult Lesson()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });

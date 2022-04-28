74f362d0da5d6a7bb7058e8e5f2decd327982c0c
robert <78978033+robertGGA@users.noreply.github.com>
2022-04-29 01:41:01 +0300

    added styles, pages and js

---
+++ Courstick/Courstick/wwwroot/js/homePageScript.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 100,  
+    autoplay: {
+        delay: 5000,
+    },
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});

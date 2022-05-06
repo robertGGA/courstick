d0161904a8123ff48bf507312f0a722f52a8050a
Rihat38 <84475346+Rihat38@users.noreply.github.com>
2022-05-06 19:12:42 +0300

    Hz

---
+++ Courstick/Infrastructure/Migrations/20220506100605_AddingCourse v0.2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Courstick.Infrastructure.Migrations
+{
+    public partial class AddingCoursev02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pages_Comments_CommentId",
+                table: "Pages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Pages",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Movie",
+                table: "Pages",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Pages",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CommentId",
+                table: "Pages",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Pages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pages_Comments_CommentId",
+                table: "Pages",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "CommentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pages_Comments_CommentId",
+                table: "Pages");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Pages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Pages",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Movie",
+                table: "Pages",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Pages",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CommentId",
+                table: "Pages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pages_Comments_CommentId",
+                table: "Pages",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "CommentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

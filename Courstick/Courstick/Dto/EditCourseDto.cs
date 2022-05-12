f281785195448a5937299f8c970602fc3162d04d
ArtemKhamzin <86554484+ArtemKhamzin@users.noreply.github.com>
2022-05-12 21:55:24 +0300

    display my courses

---
+++ Courstick/Courstick/Dto/EditCourseDto.cs
@@ -0,0 +1,17 @@
+﻿using Courstick.Core.Models;
+
+namespace Courstick.Dto
+{
+    public class EditCourseDto
+    {
+        public byte[] Image { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string SmallDescription { get; set; }
+        
+        public double Price { get; set; }
+    }
+}
---
+++ Courstick/Courstick/Dto/YourCoursesDto.cs
@@ -0,0 +1,9 @@
+﻿using Courstick.Core.Models;
+
+namespace Courstick.Views.CourseSettings
+{
+    public class YourCourseModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}

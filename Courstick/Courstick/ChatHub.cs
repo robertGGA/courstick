ad4a5597731b589680ad3d436006ba0e69b5ad91
Rihat38 <84475346+Rihat38@users.noreply.github.com>
2022-05-18 17:44:57 +0300

    SignalR v.0.1

---
+++ Courstick/Courstick/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+    }
+}
